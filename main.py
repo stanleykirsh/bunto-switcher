afa56eb0c8e4c73915b7e3671dfdb3727b7d190c
kirill <kirillvb@mail.ru>
2022-12-11 00:13:20 +0300

    добавлен gui

--- main.py
+++ main.py
@@ -1,168 +1,55 @@
-import time
-import mouse
-import keyboard
-import parameters
+#!/usr/bin/env python3.11
 
-from threading import Thread
-from xkbgroup import XKeyboard
-from parameters import RUS_CHARS, ENG_CHARS
-from parameters import SYS_SWITCH_KEY, ASWITCH_KEYS, MSWITCH_KEYS
+# Для правильной работы требуется установить расширение
+# https://extensions.gnome.org/extension/615/appindicator-support/
+# https://github.com/ubuntu/gnome-shell-extension-appindicator
+# 
+# Возможно будет достаточно просто установить пакет libappindicator-*
+# https://packages.fedoraproject.org/pkgs/libappindicator/libappindicator/
+# sudo dnf install libappindicator-gtk3
 
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
 
-class Switcher():
-    """ No comments. """
-    buffer = []
-    ngrams_ru = []
-    ngrams_en = []
+import os
+from switcher import Switcher
+from gi.repository import AppIndicator3 as appindicator
+from gi.repository import Gtk as gtk
 
-    mouse = mouse
-    keyboard = keyboard
-    xkb = XKeyboard()
-
-    def __init__(self):
-        """ No comments. """
-        self.ngrams_ru = self.load_ngrams((
-            './data/nonexistent2gram-ru-tran.txt',
-            './data/nonexistent3gram-ru-tran.txt',
-            './data/nonexistent4gram-ru-tran.txt',
-        ))
-        self.ngrams_en = self.load_ngrams((
-            './data/nonexistent2gram-en.txt',
-            './data/nonexistent3gram-en.txt',
-            './data/nonexistent4gram-en.txt',
-        ))
-
-    def load_ngrams(self, filenames):
-        """ No comments. """
-        result = []
-        for filename in filenames:
-            with open(filename, 'r') as f:
-                lines = [line.rstrip('\n') for line in f]
-                result.extend(lines)
-        return result
-
-    def ngram_contain(self, string, ngrams):
-        """ Проверяет содержится ли строка string в списке n-грам ngrams. """
-        string = string.lower()
-        for ngram in ngrams:
-            if ngram.startswith('*'):
-                if string.endswith(ngram[1:]):
-                    return True
-            elif ngram.endswith('*'):
-                if string.startswith(ngram[:-1]):
-                    return True
-            elif ngram in string:
-                return True
-        return False
-
-    def get_layout(self):
-        """ No comments. """
-        return self.xkb.group_symbol
-
-    def keyboard_type(self, text: list, delay: int = 0):
-        """ Печатает переданный список нажатий клавиш. """
-        for keys in text:
-            keyboard.send(keys)
-            time.sleep(delay)
-
-    def switch_layout(self):
-        """ No comments. """
-        keyboard.send(SYS_SWITCH_KEY)
-
-    def auto_process(self, char):
-        """ No comments. """
-        print('auto_process', char)
-
-        if char in ASWITCH_KEYS:
-            print('ASWITCH_KEYS', self.buffer)
-            string = ''.join(self.buffer)
-            initial_layout = self.get_layout()
-
-            if not (initial_layout == 'us' and self.ngram_contain(string[:-1], self.ngrams_en) or
-                    initial_layout == 'ru' and self.ngram_contain(string[:-1], self.ngrams_ru)):
-                return
-
-            for _ in self.buffer:
-                keyboard.send('backspace')
-
-            self.switch_layout()
-            self.keyboard_type(self.buffer)
-            self.keyboard.read_event()
-
-    def manual_process(self, char):
-        """ No comments. """
-        print('manual_process', char)
-
-        if char in MSWITCH_KEYS:
-
-            for _ in self.buffer:
-                keyboard.send('backspace')
-
-            self.switch_layout()
-            self.keyboard_type(self.buffer)
-            self.keyboard.read_event()
-
-    def update_buffer(self, char):
-        """ No comments. """
-        print('char =', char)
-
-        if (char in RUS_CHARS + ENG_CHARS
-                and len(self.buffer) >= 2
-                and self.buffer[-1] == ' '):
-            self.buffer.clear()
-
-        if char in RUS_CHARS + ENG_CHARS:
-            if keyboard.is_pressed('ctrl'):
-                return
-            if keyboard.is_pressed('shift'):
-                char = 'shift+' + char
-            self.buffer.append(char)
-            return
-
-        if char == 'space':
-            self.buffer.append(' ')
-            return
-
-        if char == 'backspace':
-            if self.buffer:
-                self.buffer.pop()
-            return
-
-        if (char not in RUS_CHARS + ENG_CHARS
-                and char not in ASWITCH_KEYS + MSWITCH_KEYS
-                and char not in ('ctrl+shift', 'ctrl', 'shift', 'space')):
-            self.buffer.clear()
-
-        print(self.buffer)
-
-    def on_mouse_click(self):
-        self.buffer.clear()
-
-    def on_press(self, key):
-        """ No comments. """
-        self.update_buffer(key.name)
-        if parameters.MANUAL_ENABLED:
-            self.manual_process(key.name)
-        if parameters.AUTO_ENABLED:
-            self.auto_process(key.name)
-
-    def main(self):
-        """ No comments. """
-        mouse.on_button(self.on_mouse_click)
-        while True:
-            event = keyboard.read_event()
-            if event.name == 'f12':
-                break
-            if event.event_type == keyboard.KEY_UP:
-                self.on_press(event)
-
-    def start(self):
-        """ No comments. """
-        thread = Thread(target=self.main)
-        thread.start()
+APPINDICATOR_ID = 'buntoappindicator'
 
 
-if __name__ == '__main__':
+def main():
+    icon = 'rotate-circle-white.png'
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, 
+        os.path.abspath(icon), appindicator.IndicatorCategory.APPLICATION_STATUS)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
+    gtk.main()
+
+
+def build_menu():
+    menu = gtk.Menu()
+    item_action = gtk.MenuItem.new_with_label('Настройки')
+    item_action.connect('activate', settings)
+    menu.append(item_action)
+    item_quit = gtk.MenuItem.new_with_label('Выход')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+
+def settings(source):
+    pass
+
+
+def quit(source):
+    gtk.main_quit()
+
+
+if __name__ == "__main__":
     switcher = Switcher()
     switcher.start()
-    print('started')
+    main()
---
+++ switcher.py
@@ -0,0 +1,168 @@
+import time
+import mouse
+import keyboard
+import parameters
+
+from threading import Thread
+from xkbgroup import XKeyboard
+from parameters import RUS_CHARS, ENG_CHARS
+from parameters import SYS_SWITCH_KEY, ASWITCH_KEYS, MSWITCH_KEYS
+
+
+class Switcher():
+    """ No comments. """
+    xkb = XKeyboard()
+    keyboard = keyboard
+    mouse = mouse
+
+    buffer = []
+    ngrams_ru = []
+    ngrams_en = []
+    thread = Thread()
+    supressed = False
+
+    def __init__(self):
+        """ No comments. """
+        self.ngrams_ru = self.load_ngrams((
+            './data/nonexistent2gram-ru-tran.txt',
+            './data/nonexistent3gram-ru-tran.txt',
+            './data/nonexistent4gram-ru-tran.txt',
+        ))
+        self.ngrams_en = self.load_ngrams((
+            './data/nonexistent2gram-en.txt',
+            './data/nonexistent3gram-en.txt',
+            './data/nonexistent4gram-en.txt',
+        ))
+
+    def load_ngrams(self, filenames):
+        """ No comments. """
+        result = []
+        for filename in filenames:
+            with open(filename, 'r') as f:
+                lines = [line.rstrip('\n') for line in f]
+                result.extend(lines)
+        return result
+
+    def ngram_contain(self, string, ngrams):
+        """ Проверяет содержится ли строка string в списке n-грам ngrams. """
+        string = string.lower()
+        for ngram in ngrams:
+            if ngram.startswith('*'):
+                if string.endswith(ngram[1:]):
+                    return True
+            elif ngram.endswith('*'):
+                if string.startswith(ngram[:-1]):
+                    return True
+            elif ngram in string:
+                return True
+        return False
+
+    def get_layout(self):
+        """ No comments. """
+        return self.xkb.group_symbol
+
+    def keyboard_type(self, text: list, delay: int = 0):
+        """ Печатает переданный список нажатий клавиш. """
+        for keys in text:
+            keyboard.send(keys)
+            time.sleep(delay)
+
+    def switch_layout(self):
+        """ No comments. """
+        keyboard.send(SYS_SWITCH_KEY)
+
+    def auto_process(self, char):
+        """ No comments. """
+        print('auto_process', char)
+
+        if char in ASWITCH_KEYS:
+            print('ASWITCH_KEYS', self.buffer)
+            string = ''.join(self.buffer)
+            initial_layout = self.get_layout()
+
+            if not (initial_layout == 'us' and self.ngram_contain(string[:-1], self.ngrams_en) or
+                    initial_layout == 'ru' and self.ngram_contain(string[:-1], self.ngrams_ru)):
+                return
+
+            for _ in self.buffer:
+                keyboard.send('backspace')
+
+            self.switch_layout()
+            self.keyboard_type(self.buffer)
+
+    def manual_process(self, char):
+        """ No comments. """
+        print('manual_process', char)
+
+        if char in MSWITCH_KEYS:
+            for _ in self.buffer:
+                keyboard.send('backspace')
+
+            self.switch_layout()
+            self.keyboard_type(self.buffer)
+
+    def update_buffer(self, char):
+        """ No comments. """
+        print('char =', char)
+
+        if (char in RUS_CHARS + ENG_CHARS
+                and len(self.buffer) >= 2
+                and self.buffer[-1] == ' '):
+            self.buffer.clear()
+
+        if char in RUS_CHARS + ENG_CHARS:
+            if keyboard.is_pressed('ctrl'):
+                return
+            if keyboard.is_pressed('shift'):
+                char = 'shift+' + char
+            self.buffer.append(char)
+            return
+
+        if char == 'space':
+            self.buffer.append(' ')
+            return
+
+        if char == 'backspace':
+            if self.buffer:
+                self.buffer.pop()
+            return
+
+        if (char not in RUS_CHARS + ENG_CHARS
+                and char not in ASWITCH_KEYS + MSWITCH_KEYS
+                and char not in ('ctrl+shift', 'ctrl', 'shift', 'space')):
+            self.buffer.clear()
+
+        print(self.buffer)
+
+    def on_mouse_click(self):
+        self.buffer.clear()
+
+    def on_release(self, key):
+        """ No comments. """
+        if self.supressed:
+            return
+        self.lock_listener()
+        self.update_buffer(key.name)
+        if parameters.MANUAL_ENABLED:
+            self.manual_process(key.name)
+        if parameters.AUTO_ENABLED:
+            self.auto_process(key.name)
+        self.unlock_listener()
+
+    def lock_listener(self):
+        self.supressed = True
+
+    def unlock_listener(self):
+        def _unlock():
+            self.supressed = False
+        keyboard.call_later(fn=_unlock, delay=0.01)
+
+    def main(self):
+        """ No comments. """
+        mouse.on_button(self.on_mouse_click)
+        keyboard.on_release(self.on_release)
+
+    def start(self):
+        """ No comments. """
+        self.thread = Thread(target=self.main)
+        self.thread.start()
