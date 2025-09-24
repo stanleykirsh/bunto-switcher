4e1c6020324639e10c16e2134eb82ec542e7bf07
stanleykirsh <kirillvb@mail.ru>
2025-09-25 00:38:01 +0300

    - вызов перерисовки индикатора языка в асинхронном режиме для ускорения и плавности - почищен код

--- settings.py
+++ settings.py
@@ -12,6 +12,17 @@ SWITCH_TWOCAPS = True
 # Сочетание клавиш на которую в системе завязано переключение раскладки.
 SYS_SWITCH_KEY = ["ctrl_left", "shift_left"]
 
+# Символы кототрые являются признаком заверешения ввода слова.
+# space = 57, tab = 15, enter = 28
+# {'space': ' ', 'tab': '\t', 'enter': '\r\n'}
+EOW_KEY_CODES = [57, 15, 28]
+
+# Клавиши по которым происходит автоматическое переключение раскладки если SWITCH_AUTO = True.
+ASWITCH_KEY_CODES = [57, 15] # space, tab
+
+# Клавиши по которым происходит ручное переключение раскладки если SWITCH_MANUAL = True.
+MSWITCH_KEY_CODES = [119] # pause
+
 # Приложения исключения
 APP_EXCEPTIONS = '''
 firewatch
--- switcher.py
+++ switcher.py
@@ -3,16 +3,9 @@ gi.require_version('Gtk', '3.0')
 
 from mouse.mouse import Mouse
 from keyboard.keyboard import Keyboard
-# from clipboard.clipboard import Clipboard
+from threading import Timer
+from keyboard.keymap import (EV_KEYS, VIS_KEYS)
 from gi.repository import Gtk as gtk
-# from settings import SYS_SWITCH_KEY
-# from threading import Timer
-from charmap import (
-    EOW_KEY_CODES,
-    ASWITCH_KEY_CODES,
-    MSWITCH_KEY_CODES,
-    # INSERT_KEY_CODE,
-    KEY_MAP)
 
 import os
 import settings
@@ -24,7 +17,6 @@ class Switcher():
 
     def __init__(self):
         """"""
-        # self.clipboard = Clipboard()
         self.keyboard = Keyboard()
         self.mouse = Mouse()
 
@@ -38,10 +30,9 @@ class Switcher():
 
         self.username = os.environ['SUDO_USER']
         self.useruid = self.get_user_uid(self.username)
+        
         self.initial_layout = self.get_layout()
-        # print("initial_layout", self.initial_layout)
-
-        self.PRESSED = False
+        self.STILL_PRESSED = False
 
     def load_ngrams(self, filenames):
         """"""
@@ -54,38 +45,33 @@ class Switcher():
 
     def get_layout_probability(self, string: str):
         """"""
-        LITERALS = '''qwertyuiopasdfghjklzxcvbnmёйцукенгшщзхъфывапролджэячсмитьбю'''
-
-        string = string.lower()
-        string = ''.join([s for s in string if s in LITERALS])
+        LITERALS = 'qwertyuiopasdfghjklzxcvbnmёйцукенгшщзхъфывапролджэячсмитьбю'
+        string = ''.join(filter(LITERALS.__contains__, string.lower()))
 
         # Для слов исключений вероятность языка неопределенная.
         # Менять раскладку автоматически для них не требуется.
         if string.strip() in settings.IGNORE_WORDS.splitlines():
             return ''
 
-        prob_ru = sum((1 for ng in self.ngrams_ru if ng in string))
-        prob_en = sum((1 for ng in self.ngrams_en if ng in string))
+        prob_ru = sum(ng in string for ng in self.ngrams_ru)
+        prob_en = sum(ng in string for ng in self.ngrams_en)
 
-        if prob_ru > prob_en: return 'ru'
-        if prob_ru < prob_en: return 'us'
-        if prob_ru == prob_en: return ''
+        return 'ru' if prob_ru > prob_en else 'us' if prob_ru < prob_en else ''
+
 
     def get_layout(self):
         """"""
-        get_mru_sources = f'sudo -u {self.username} gsettings get org.gnome.desktop.input-sources mru-sources'.split()
-        result = subprocess.run(get_mru_sources, stdout=subprocess.PIPE)
-        result = result.stdout.decode('utf-8')[10:12]
-        self.initial_layout = result
-        return result
+        commands = f'sudo -u {self.username} gsettings get org.gnome.desktop.input-sources mru-sources'.split()
+        return subprocess.run(commands, stdout=subprocess.PIPE).stdout.decode('utf-8')[10:12]
 
     def get_target_layout(self):
         """"""
-        # print('get_target_layout', self.buffer, self.initial_layout)
-        string = self.decode_buffer()
-        string = string.replace('\t', ' ')
-        string = string.replace('\r\n', ' ')
-        return self.get_layout_probability(string)
+        string = self.decode_buffer('us')
+        string = string.replace('\t', ' ').replace('\r\n', ' ')
+        layout = self.get_layout_probability(string)
+        if (layout == 'ru' and self.initial_layout == 'us'): return 'ru'
+        if (layout == 'us' and self.initial_layout == 'ru'): return 'us'
+        return self.initial_layout
 
     def get_user_uid(self, user):
         """"""
@@ -98,9 +84,8 @@ class Switcher():
         )
         return result.stdout.strip()
 
-
     def set_layout(self, layout: str):
-        """"""        
+        """"""
         if layout == 'ru':
             str_0 = "[('xkb','ru')]"
             str_1 = "[('xkb','ru'),('xkb','us')]"
@@ -114,11 +99,12 @@ class Switcher():
 
         # Выполняем команды с указанием D-Bus адреса
         # Объединяем все команды в одну строку
-        commands = (
+        commands_0 = (
             f"""sudo -u {self.username} """
             f"""env DBUS_SESSION_BUS_ADDRESS={dbus_address} """
-            f"""gsettings set org.gnome.desktop.input-sources sources "{str_0}"; """
-            
+            f"""gsettings set org.gnome.desktop.input-sources sources "{str_0}"; """)
+
+        commands_1 = (
             f"""sudo -u {self.username} """
             f"""env DBUS_SESSION_BUS_ADDRESS={dbus_address} """
             f"""gsettings set org.gnome.desktop.input-sources mru-sources "{str_1}"; """
@@ -127,18 +113,22 @@ class Switcher():
             f"""env DBUS_SESSION_BUS_ADDRESS={dbus_address} """
             f"""gsettings set org.gnome.desktop.input-sources sources "{str_1}\"""")
 
-        # Выполняем объединённые команды
+        # выполняем объединённые команды синхронно
         subprocess.run(
-            ["bash", "-c", commands],
-            # check=True,
+            ["bash", "-c", commands_0],
             shell=False
         )
+
+        # выполняем объединённые команды асинхронно
+        Timer(0.2, subprocess.run, kwargs={
+            'args': ["bash", "-c", commands_1],
+            'shell': False}).start()
 
     def kb_auto_process(self, key_code: int):
         """"""
         if (
             not self.buffer
-            or key_code not in ASWITCH_KEY_CODES
+            or key_code not in settings.ASWITCH_KEY_CODES
         ):
             return
 
@@ -152,20 +142,20 @@ class Switcher():
         if all(code[2:4] in ('00', '11') for code in self.buffer):
             return
         
-        self.initial_layout = target_layout        
+        self.initial_layout = target_layout
         self.set_layout(target_layout)
         self.delete_last_word()
         self.type_buffer()
 
         # корректировка состояния если нажатая клавиша еще не отжата
         if self.keyboard.is_pressed(key_code):
-            self.PRESSED = True
+            self.STILL_PRESSED = True
 
     def kb_manual_process(self, key_code: int):
         """"""
         if (
             not self.buffer
-            or key_code not in MSWITCH_KEY_CODES
+            or key_code not in settings.MSWITCH_KEY_CODES
         ):
             return
 
@@ -177,11 +167,15 @@ class Switcher():
         self.delete_last_word()
         self.type_buffer()
 
+        # корректировка состояния если нажатая клавиша еще не отжата
+        if self.keyboard.is_pressed(key_code):
+            self.STILL_PRESSED = True
+
     def caps_auto_process(self, key_code: int):
         """"""
         if (
             not self.buffer
-            or key_code not in MSWITCH_KEY_CODES + ASWITCH_KEY_CODES            
+            or key_code not in settings.MSWITCH_KEY_CODES + settings.ASWITCH_KEY_CODES
         ):
             return
 
@@ -201,14 +195,18 @@ class Switcher():
         self.delete_last_word()
         self.type_buffer()
 
+        # корректировка состояния если нажатая клавиша еще не отжата
+        if self.keyboard.is_pressed(key_code):
+            self.STILL_PRESSED = True
+
     def upper_fix_required(self):
         """"""
-        string = self.decode_buffer()
+        string = self.decode_buffer('us')
         # ИСправление
         if (len(string) >= 2
             and string[0:2].isupper()
             and not string.isupper()):
-            return True        
+            return True
         # исПРАВЛЕНИЕ
         if (len(string) >= 2
             and string[0:2].islower()
@@ -224,7 +222,7 @@ class Switcher():
     def type_buffer(self):
         """"""
         for key in self.buffer:
-            if int(key[3]):
+            if int(key[3]) != int(key[4]): # капс
                 self.keyboard.press(42)
                 self.keyboard.send([int(key[:3])])
                 self.keyboard.release(42)
@@ -233,41 +231,32 @@ class Switcher():
 
     def encode_key(self, key_code):
         """"""
-        encoded = ('00' + str(key_code))[-3:] + '00'
-        encoded_list = list(encoded)
-
         # shift_left = 42, shift_right = 54
-        if self.keyboard.is_pressed(42) or self.keyboard.is_pressed(54):
-            encoded_list[3] = '1'
+        encoded = list(f"{key_code:03d}00")        
+        encoded[3] = '1' if self.keyboard.is_pressed(42) or self.keyboard.is_pressed(54) else '0'
+        encoded[4] = '1' if self.keyboard.is_caps_locked() else '0'        
+        return ''.join(encoded)
 
-        if self.keyboard.is_caps_locked():
-            encoded_list[4] = '1'
-
-        return "".join(encoded_list)
-
-    def decode_buffer(self, target_layout='us'):
+    def decode_buffer(self, layout='us'):
         """"""
-        decoded_str = ""
-        for key in self.buffer:
-
-            key_code = int(key[:3])
-            is_capitalized = bool(int(key[3]) != int(key[4]))
-
-            _target_layout = target_layout + '_' if is_capitalized else target_layout
-
-            if key_code in KEY_MAP:
-                char = KEY_MAP[key_code][_target_layout]
-
-            decoded_str += char
-        return decoded_str
+        string = ''
+        for v in self.buffer:
+            for k in EV_KEYS:
+                if int(v[:3]) == k[5]:
+                    if v[3] != v[4]: # капс
+                        string += k[2] if layout == 'us' else k[4]                        
+                    else:
+                        string += k[1] if layout == 'us' else k[3]
+                    continue
+        return string
 
     def update_buffer(self, key_code):
         """"""
         # Не многоязыковая клавиша или не клавиша переключения
         if (
-                key_code not in KEY_MAP
-                and key_code not in ASWITCH_KEY_CODES
-                and key_code not in MSWITCH_KEY_CODES
+                key_code not in VIS_KEYS
+                and key_code not in settings.ASWITCH_KEY_CODES
+                and key_code not in settings.MSWITCH_KEY_CODES
                 and key_code not in (29, 97, 42, 54, 57, 58)
                 # ctrl_left, ctrl_right, shift_left, shift_right, space, caps_lock
             ):
@@ -284,18 +273,18 @@ class Switcher():
         # Первое условие обязательно первое чтобы при пустом буфере не падало второе.
         if (
             self.buffer
-            and int(self.buffer[-1][:3]) in EOW_KEY_CODES
-            and (key_code in KEY_MAP or key_code in EOW_KEY_CODES)
+            and int(self.buffer[-1][:3]) in settings.EOW_KEY_CODES
+            and (key_code in VIS_KEYS or key_code in settings.EOW_KEY_CODES)
         ):
             self.buffer.clear()
 
         # Многоязыковая клавиша
-        if key_code in KEY_MAP:
+        if key_code in VIS_KEYS:
             self.buffer.append(self.encode_key(key_code))
             return
 
         # Конец слова
-        if key_code in EOW_KEY_CODES:
+        if key_code in settings.EOW_KEY_CODES:
             self.buffer.append(self.encode_key(key_code))
             return
 
@@ -307,10 +296,10 @@ class Switcher():
 
     def on_mouse_click(self, event):
         """"""
-        self.get_layout()
+        self.initial_layout = self.get_layout()
         self.buffer.clear()
 
-    def on_key_pressed(self, event):
+    def is_pressed(self, event):
         """"""
         key_char = str(event.key_char)
         key_code = int(event.key_code)
@@ -327,17 +316,17 @@ class Switcher():
 
         if event.type == 'up':
             # корректировка состояния если нажатая клавиша еще не отжата
-            if self.PRESSED:
-                self.PRESSED = False
+            if self.STILL_PRESSED:
+                self.STILL_PRESSED = False
                 self.keyboard.send([key_code])        
             # обновляем текущую раскладку при ручном переключении
             if key_char in (settings.SYS_SWITCH_KEY):
-                self.get_layout()
+                self.initial_layout = self.get_layout()
 
     def start(self):
         """"""
         self.mouse.on_button_event(self.on_mouse_click)
-        self.keyboard.on_key_event(self.on_key_pressed)
+        self.keyboard.on_key_event(self.is_pressed)
 
 
 switcher = Switcher()
